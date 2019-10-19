26ae8121ddf20f651ebd980035ff9a3bfb0742cb
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-10-19 17:45:53 +0200

    Remove input highlighting (clunky)

--- src/CSharpToTypeScript.Web/wwwroot/js/site.ts
+++ src/CSharpToTypeScript.Web/wwwroot/js/site.ts
@@ -1,14 +1,6 @@
-declare const Prism: any;
-
-const inputCodeHighlightedContainer = document.getElementById('inputCodeHighlightedContainer')!;
-const inputCodeHighlighted = inputCodeHighlightedContainer.querySelector('code')!;
 const inputCodeTextarea = document.getElementById('InputCode') as HTMLTextAreaElement;
 const convertedCodeHiddenInput = document.getElementById('convertedCodeHiddenInput') as HTMLTextAreaElement;
 const convertedCodeContainer = document.getElementById('convertedCodeContainer')!;
-
-inputCodeHighlightedContainer.addEventListener('click', editInputCode);
-
-inputCodeTextarea.addEventListener('blur', highlightInputCode)
 
 inputCodeTextarea.addEventListener('keydown', ev => {
     if (ev.keyCode === 9 /* Tab */) {
@@ -30,22 +22,6 @@ convertedCodeContainer.addEventListener('click', () => {
 })
 
 convertedCodeContainer.addEventListener('animationend', cleanUpAnimations);
-
-function editInputCode() {
-    inputCodeHighlightedContainer.hidden = true;
-    inputCodeTextarea.hidden = false;
-
-    inputCodeTextarea.focus();
-}
-
-function highlightInputCode() {
-    inputCodeHighlightedContainer.hidden = false;
-    inputCodeTextarea.hidden = true;
-
-    inputCodeHighlighted.textContent = inputCodeTextarea.value;
-
-    Prism.highlightElement(inputCodeHighlighted);
-}
 
 function copyToClipboard() {
     convertedCodeHiddenInput.hidden = false;
