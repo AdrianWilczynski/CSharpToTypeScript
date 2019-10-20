f4f96a1a9de0cf909c16619a01454528e65e4944
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-10-20 12:22:13 +0200

    Add missing "call"

--- tools/install-extension.cmd
+++ tools/install-extension.cmd
@@ -1,4 +1,4 @@
 pushd ..\src\CSharpToTypeScript.VSCodeExtension
 call vsce package
-code --install-extension csharp-to-typescript-%1.vsix
+call code --install-extension csharp-to-typescript-%1.vsix
 popd
