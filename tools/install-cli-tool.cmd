17e193aeabd718d8044d0e6a123d796b603e45be
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-10-20 12:13:53 +0200

    Set package properties. Add uninstall step to CLI tool install script.

--- tools/install-cli-tool.cmd
+++ tools/install-cli-tool.cmd
@@ -1,4 +1,5 @@
 pushd ..\src\CSharpToTypeScript.CLITool
 dotnet pack
+dotnet tool uninstall --global CSharpToTypeScript.CLITool
 dotnet tool install --global --add-source ./nupkg CSharpToTypeScript.CLITool
 popd
