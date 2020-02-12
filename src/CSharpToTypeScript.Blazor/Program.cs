138500279f2620121e932b2a23ad4e4092e20650
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2020-02-12 13:58:31 +0100

    Add proof of concept for a Blazor app.

---
+++ src/CSharpToTypeScript.Blazor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using CSharpToTypeScript.Core.DependencyInjection;
+
+namespace CSharpToTypeScript.Blazor
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddCSharpToTypeScript();
+
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
