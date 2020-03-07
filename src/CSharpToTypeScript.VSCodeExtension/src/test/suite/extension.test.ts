8baa9bc4582b8406964a06eec330db4e3d167d57
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2020-03-07 20:34:02 +0100

    Add test for replacing selection

--- src/CSharpToTypeScript.VSCodeExtension/src/test/suite/extension.test.ts
+++ src/CSharpToTypeScript.VSCodeExtension/src/test/suite/extension.test.ts
@@ -6,8 +6,8 @@ suite('Commands Tests', () => {
     test('Replace whole document', async () => {
         const document = await vscode.workspace.openTextDocument({
             language: 'csharp',
-            content: `
-                using System;
+            content:
+                `using System;
 
                 namespace MyProject.DTOs
                 {
@@ -24,5 +24,40 @@ suite('Commands Tests', () => {
 
         assert.ok(convertedContent.includes('interface Item {'));
         assert.ok(convertedContent.includes(': string;'));
+    });
+
+    test('Replace selection', async () => {
+        const document = await vscode.workspace.openTextDocument({
+            language: 'csharp',
+            content:
+                `using System;
+
+                namespace MyProject.DTOs
+                {
+                    public class Keep
+                    {
+                        public string KeepText { get; set; }
+                    }
+
+                    public class Replace
+                    {
+                        public int ReplaceNumber { get; set; }
+                    }
+                }`});
+        await vscode.window.showTextDocument(document);
+
+        vscode.window.activeTextEditor!.selection = new vscode.Selection(
+            9, 0,
+            document.lineCount - 1, document.lineAt(document.lineCount - 1).range.end.character);
+
+        await vscode.commands.executeCommand('csharpToTypeScript.csharpToTypeScriptReplace');
+
+        const convertedContent = document.getText();
+
+        assert.ok(convertedContent.includes('class Keep'));
+        assert.ok(convertedContent.includes('public string KeepText { get; set; }'));
+
+        assert.ok(convertedContent.includes('interface Replace'));
+        assert.ok(convertedContent.includes(': number;'));
     });
 });
