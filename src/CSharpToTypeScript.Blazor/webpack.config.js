eb4d172d6a73081a47cbb9b2ab8c1e18627ebf4c
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2020-03-16 15:23:37 +0100

    Only include C# and TypeScript support

--- src/CSharpToTypeScript.Blazor/webpack.config.js
+++ src/CSharpToTypeScript.Blazor/webpack.config.js
@@ -34,7 +34,9 @@ const config = {
         ]
     },
     plugins: [
-        new MonacoWebpackPlugin()
+        new MonacoWebpackPlugin({
+            languages: ['csharp', 'typescript']
+        })
     ],
     output: {
         filename: '[name].js',
