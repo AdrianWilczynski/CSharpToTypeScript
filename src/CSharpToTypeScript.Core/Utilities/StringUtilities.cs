ea2c3dc22853332e58aa52ffd6a73aa8e1427141
Seethoven <34771525+ryanryanorient@users.noreply.github.com>
2022-12-22 16:29:57 +0800

    Update StringUtilities.cs
    Fix ToCamelCase

--- src/CSharpToTypeScript.Core/Utilities/StringUtilities.cs
+++ src/CSharpToTypeScript.Core/Utilities/StringUtilities.cs
@@ -31,9 +31,38 @@ namespace CSharpToTypeScript.Core.Utilities
             };
 
         public static string ToCamelCase(this string text)
-            => !string.IsNullOrEmpty(text) ?
-            Regex.Replace(text, "^[A-Z]", char.ToLowerInvariant(text[0]).ToString())
-            : text;
+        {
+            if (string.IsNullOrEmpty(text) || !char.IsUpper(text[0]))
+            {
+                return text;
+            }
+
+            char[] chars = text.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (i == 1 && !char.IsUpper(chars[i]))
+                {
+                    break;
+                }
+
+                bool hasNext = (i + 1 < chars.Length);
+                
+                if (i > 0 && hasNext && !char.IsUpper(chars[i + 1]))
+                {
+                    if (chars[i + 1] == ' ')
+                    {
+                        chars[i] = char.ToLowerInvariant(chars[i]);
+                    }
+
+                    break;
+                }
+
+                chars[i] = char.ToLowerInvariant(chars[i]);
+            }
+
+            return new string(chars);
+        }
 
         public static string ToKebabCase(this string text)
             => Regex.Replace(text, "(?<![A-Z]|^)([A-Z])", "-$1").ToLowerInvariant();
